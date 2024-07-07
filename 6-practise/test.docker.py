c23c8b74161512ed1ae2a499768737719c650e7a
Olawale Ogundeji <abiodungndj@gmail.com>
2024-07-07 14:25:08 +0100

    Updated

---
+++ 6-practise/test.docker.py
@@ -0,0 +1,24 @@
+import requests
+
+event = {
+    "Records": [
+        {
+            "kinesis": {
+                "kinesisSchemaVersion": "1.0",
+                "partitionKey": "1",
+                "sequenceNumber": "495995532424900000000000000000000000",
+                "data": "SGVsbG8sIFdvcmxkIQ==",
+                "approximateArrivalTimestamp": 1547396096.0
+            }
+            "eventSource": "aws:kinesis",
+            "eventVersion": "1.0"
+            "eventID": "12345",
+            "eventName": "aws:kinesis:record",
+            "InvokeIdentityArn": "aws:kinesis:iam::role/lambda-kinesis
+            "awsRegion": "eu-west-2",
+            "eventSourceARN": "arn:aws:kinesis:eu-west-2:387546586013 ",
+            "functionArn
+        }
+    ]
+        
+}
